0c7aa38d8938048be0283de52291b4f0aedf73a5
jehkke <jbndoit@gmail.com>
2023-09-21 16:24:43 +0900

    3

--- main.c
+++ main.c
@@ -5,13 +5,10 @@
 
 int main(void) {
 	char c;
-	int i;
 	
-	printf("input a number :");
+	printf("input a character :");
 	scanf("%c", &c);
 	
-	i= c - '0';
-	printf("The input number is %i\n",i);
-	
-	system("PAUSE")
+	printf("The next character of %c(%d) is %c (%d)\n", c, c, c+1, c+1);
+	system("PAUSE");
 }
